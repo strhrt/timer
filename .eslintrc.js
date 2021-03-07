acfa8c58b94b2f08d3b4666dcf978d9d6d2d69d3
Diana Mamedova <ddmamedo@mts.ru>
2021-03-07 21:17:46 +0300

    feat(eslint): update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'comma-dangle': ['error', 'never'],
-    semi: ['error', 'never']
+    semi: ['error', 'never'],
+    'max-len': ['error', { ignoreComments: true }, { code : 120 } ]
   }
 }
